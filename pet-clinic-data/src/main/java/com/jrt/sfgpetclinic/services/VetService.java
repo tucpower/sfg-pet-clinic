4a60b699083ffe1fed25771cfdc963f7cd665f1d
Nikos Giannopoulos <n.giannopoulos@jrtechnologies.com>
2018-07-31 10:15:53 +0300

    After Lecture 68.

--- pet-clinic-data/src/main/java/com/jrt/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/jrt/sfgpetclinic/services/VetService.java
@@ -2,14 +2,6 @@ package com.jrt.sfgpetclinic.services;
 
 import com.jrt.sfgpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long>{
 
 }
